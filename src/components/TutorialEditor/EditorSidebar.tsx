2350952ab19e356d7f208b38db2806befed57e2f
darkysadow <oleksandr.poberezhniak@gmail.com>
2024-05-21 19:00:22 +0300

    Created sidebar on TutorialEditor page

---
+++ src/components/TutorialEditor/EditorSidebar.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { useAppSelector } from 'src/redux/hooks';
+import { RootState } from 'src/redux/store';
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from '../ui/Accordion';
+
+interface EditorSidebarProps {
+    tutorialTitle: string;
+}
+
+const EditorSidebar = (props: EditorSidebarProps) => {
+    const chapters = useAppSelector(
+        (state: RootState) => state.editor.chapters
+    );
+    return (
+        <div className="relative w-1/4 text-white">
+            <div className="sticky top-0">
+                <div className="py-10 font-RobotoSlab text-2xl font-medium">
+                    Index
+                </div>
+                <div className="flex flex-col">
+                    <h2 className="text-xl leading-8">
+                        {props.tutorialTitle.replaceAll('\\s+', '') === ''
+                            ? 'Tutorial Title'
+                            : props.tutorialTitle}
+                    </h2>
+                    <Accordion type="single" collapsible>
+                        {chapters.map((chapter, index) => (
+                            <AccordionItem
+                                value={`chapter-item-${index}`}
+                                key={index}
+                            >
+                                {chapter.subchapters.length > 0 ? (
+                                    <>
+                                        <AccordionTrigger>
+                                            {chapter.title !== ''
+                                                ? chapter.title
+                                                : `Chapter ${index + 1}`}
+                                        </AccordionTrigger>
+                                        <AccordionContent>
+                                            <ul className="mt-2 flex flex-col gap-y-2 pl-8">
+                                                {chapter.subchapters.map(
+                                                    (subchapter, index) => (
+                                                        <li
+                                                            key={index}
+                                                            className="cursor-pointer hover:underline"
+                                                        >
+                                                            {subchapter.title !==
+                                                            ''
+                                                                ? subchapter.title
+                                                                : `Subchapter ${
+                                                                      index + 1
+                                                                  }`}
+                                                        </li>
+                                                    )
+                                                )}
+                                            </ul>
+                                        </AccordionContent>
+                                    </>
+                                ) : (
+                                    <div className="mt-6 flex flex-1 cursor-pointer items-center justify-between pl-6 text-sm font-bold transition-all hover:underline [&[data-state=open]>svg]:rotate-180">
+                                        {chapter.title !== ''
+                                            ? chapter.title
+                                            : `Chapter ${index + 1}`}
+                                    </div>
+                                )}
+                            </AccordionItem>
+                        ))}
+                    </Accordion>
+                </div>
+            </div>
+            <div className="absolute right-0 top-0 -z-10 h-full w-[300%] bg-secondary-navy"></div>
+        </div>
+    );
+};
+
+export default EditorSidebar;
--- src/components/TutorialEditor/TutorialEditor.tsx
+++ src/components/TutorialEditor/TutorialEditor.tsx
@@ -7,6 +7,7 @@ import TutorialButtonsSection from './TutorialButtonsSection';
 import AddChapterSection from './AddChapterSection';
 import { ChapterInterface } from 'src/types/types';
 import ChapterSection from './ChapterSection';
+import EditorSidebar from './EditorSidebar';
 
 const BlogEditor = () => {
     const tutorialTitle = useAppSelector(
@@ -18,17 +19,7 @@ const BlogEditor = () => {
 
     return (
         <main className="container mx-auto flex flex-auto flex-row justify-between">
-            <div className="relative w-1/4 text-white">
-                <p>Index</p>
-                <div className="flex flex-col">
-                    <h1>
-                        {tutorialTitle.replaceAll('\\s+', '') === ''
-                            ? 'Tutorial Title'
-                            : tutorialTitle}
-                    </h1>
-                </div>
-                <div className="absolute right-0 top-0 -z-10 h-full w-[300%] bg-secondary-navy"></div>
-            </div>
+            <EditorSidebar tutorialTitle={tutorialTitle} />
             <div className="flex w-3/4 flex-col items-start pl-4">
                 <TutorialButtonsSection />
                 <TutorialTopSection tutorialTitle={tutorialTitle} />
