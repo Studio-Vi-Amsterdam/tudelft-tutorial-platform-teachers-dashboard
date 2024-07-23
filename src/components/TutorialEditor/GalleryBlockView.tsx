63ef4f0ebb8a156ab257f1364899faf2d4d6a96d
darkysadow <oleksandr.poberezhniak@gmail.com>
2024-07-23 16:10:04 +0300

    Fix bug with video editing. Fix GalleryListView

--- src/components/TutorialEditor/GalleryBlockView.tsx
+++ src/components/TutorialEditor/GalleryBlockView.tsx
@@ -14,7 +14,7 @@ const GalleryBlockView = (props: GalleryViewProps) => {
           className={`
             ${
               selectedMedia === item ? '' : 'before:!hidden'
-            } ${props.selectMode ? 'after:w-6 after:h-6 after:absolute after:border-2 after:bg-white after:rounded-sm after:border-stone after:top-2 after:right-2 after:z-10' : ' '} 
+            } ${props.selectMode ? 'after:w-6 after:h-6 after:absolute after:border-2 after:bg-white after:rounded-sm after:border-stone after:top-2 after:right-2 after:z-10' : ' group '} 
           ${props.mediaToDelete !== undefined && props.mediaToDelete.includes(item) && 'after:!bg-primary-skyBlue after:!border-primary-skyBlue after:!opacity-90 after:!bg-check after:!bg-center after:!bg-no-repeat'}
           relative  before:absolute before:left-0 before:top-0 before:h-full before:w-full before:bg-black before:opacity-50 before:z-10`}
           onClick={
@@ -23,6 +23,13 @@ const GalleryBlockView = (props: GalleryViewProps) => {
               : () => handleSelectMedia(item)
           }
         >
+          {!props.selectMode && (
+            <div className="absolute w-full h-full bg-[rgba(0,0,0,0.3)] opacity-0 group-hover:opacity-100 transition-opacity flex justify-center items-center">
+              <div className="border-transparent bg-primary-skyBlue z-10 text-primary-white disabled:bg-tertiary-skyBlue-20 flex flex-row items-center justify-center gap-x-6 rounded-[4px] border px-6 py-3 [&>div]:text-2xl transition-colors duration-200 [&>div]:leading-6 [&>p]:text-base">
+                Edit
+              </div>
+            </div>
+          )}
           {props.hideVideo && <div className="absolute top-0 left-0 w-full h-full z-10"></div>}
 
           <MediaPreviewTemplate item={item} styles="w-full" />
