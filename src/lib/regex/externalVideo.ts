c087d6092b9e9d4b7368cff8525cf34cc1dc0f96
darkysadow <oleksandr.poberezhniak@gmail.com>
2024-11-05 18:08:54 +0200

    Added ExternalVideo component

---
+++ src/lib/regex/externalVideo.ts
@@ -0,0 +1,4 @@
+// eslint-disable-next-line no-useless-escape
+export const urlPattern =
+  /^(https?:\/\/)?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/|vimeo\.com\/)\w+/i
+export const iframePattern = /<iframe.*?<\/iframe>/i
