7ab225c8ac6cad787cc1fa136befe8e53af18233
Riccardo Malavolti <riccardo.malavolti@roguewavesai.com>
2025-10-29 17:32:18 +0100

    chore: update README and adds build script

---
+++ deploy/build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+echo "Installing dependencies..."
+npm install
+
+echo "Production optimized build...."
+NODE_ENV=production npm run build
+
+echo "Docker image building...."
+docker build -f deploy/Dockerfile . -t tudelft/tutorial-platform-teachers-dashboard
+
+echo "Done."
